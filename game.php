a54fdfb80eaac76e37e21e3caa3b6bda86fb76b6
adaskie3 <aj91266@stud.uws.edu.pl>
2025-06-17 15:55:45 +0200

    Update game.php
    Dodanie <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Exo+2:wght@300;600&display=swap" rel="stylesheet" /> dla identycznej czcionki loga na stronie gry.

--- game.php
+++ game.php
@@ -70,6 +70,7 @@ if (isset($_SESSION['user_id'])) {
     <link rel="icon" type="image/jpeg" href="img/favicon.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&family=Exo+2:wght@300;600&display=swap" rel="stylesheet" />
     <link href="css/game.css" rel="stylesheet" />
 </head>
 <body class="bg-space">
